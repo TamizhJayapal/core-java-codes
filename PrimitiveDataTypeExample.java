6a34c55b1fb1126b3d694e413d5c1cc53fc98e65
Tamizhazhagan Jayapal <40054684+TamizhJayapal@users.noreply.github.com>
2023-10-13 01:20:26 +0530

    Create PrimitiveDataTypeExample.java
    Sample codes added for primitive datatypes

---
+++ PrimitiveDataTypeExample.java
@@ -0,0 +1,26 @@
+/**
+      java has 8 primitive data types
+      To store single simple data/value
+      byte  - 8bit
+      short - 16bit
+      int - 32bit
+      long - 64bit
+      float - 32bit
+      double - 64bit
+      char - 16bit
+      boolean - 1bit
+      **/
+
+public class PrimitiveDataTypeExample {
+    public static void main(String[] arg){
+      byte a = 64;
+      short b = 32000;
+      int c = 640000;
+      long d = 120000000L;
+      float f = 12.123456F;
+      double g = 12.12345678901234;
+      char h = 'A';
+      boolean i = true;
+      System.out.println(a);
+    }
+}
