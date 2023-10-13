9d0d7136a11ef2ecead42dea2600acb0ba57a339
Tamizhazhagan Jayapal <40054684+TamizhJayapal@users.noreply.github.com>
2023-10-14 00:46:59 +0530

    Create SortingArray.java

---
+++ SortingArray.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class SortingArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter the array size");
+		
+		int size = scanner.nextInt();
+		
+		int[] numbers = new int[size];
+		
+		for(int i=0; i<size; i++) {
+			System.out.println("Enter the element "+i+":");
+			numbers[i] = scanner.nextInt();
+		}
+		
+		
+		for(int j=0; j < numbers.length-1;j++) {
+			for(int k=j+1; k<numbers.length; k++) {
+				if(numbers[j] > numbers[k]) {
+					int temp = numbers[j];
+					numbers[j] = numbers[k];
+					numbers[k] = temp;
+				}
+			}
+		}
+
+		System.out.println();
+		for(int m: numbers) {
+			System.out.print(m+" ");
+		}
+
+	}
+
+}
