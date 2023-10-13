cb8a80f2bf5e06567b9e6e6f29bea253d36b10fd
Tamizhazhagan Jayapal <40054684+TamizhJayapal@users.noreply.github.com>
2023-10-14 01:02:39 +0530

    Update Main.java

--- ArithmeticCalculator/Main.java
+++ ArithmeticCalculator/Main.java
@@ -2,6 +2,25 @@ package com.calculator;
 
 import java.util.Scanner;
 
+class Calculate {
+	public static void addition(int a, int b) {
+		System.out.println("result is: ");
+		System.out.println(a+b);
+	}
+	public static void substraction(int a, int b) {
+		System.out.println("result is: ");
+		System.out.println(a-b);
+	}
+	public static void multiplication(int a, int b) {
+		System.out.println("result is: ");
+		System.out.println(a*b);
+	}
+	public static void divition(int a, int b) {
+		System.out.println("result is: ");
+		System.out.println(a/b);
+	}
+}
+
 public class Main {
 
 	public static void main(String[] args) {
