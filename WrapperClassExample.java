8abf68aef79090e759eb1db67e5271fafffb81c2
Tamizhazhagan Jayapal <40054684+TamizhJayapal@users.noreply.github.com>
2023-10-28 16:39:48 +0530

    Update WrapperClassExample.java

--- WrapperClassExample.java
+++ WrapperClassExample.java
@@ -1,4 +1,3 @@
-
 /*
 Wrapper classes is used to reperesent the primitive types as object.
 And it provides method to work with object oriented maner.
@@ -25,20 +24,50 @@ methods:
 4.parseInt
 5.equals
 6.compareTo
-7.hashCode
-8.isNan
-9.MIN_VALUE
-10.MAX_VALUE
+7.isNaN
+8.MIN_VALUE
+9.MAX_VALUE
 
 */
 
+package com.example;
 
-public class Main {
+public class WrapperClassExample {
     public static void main(String[] args) {
         int i = 10;
+        
         //boxing
-        Integer i1 = new Integer(i);
-        i1.valueOf(i);
-        System.out.println(i1.intValue());
+        Integer i1 = new Integer(2);
+       // Integer.valueOf(i);
+        
+       // AutoBoxing
+        Integer i2 = 20;
+        System.out.println(i2.intValue());
+        
+        // unboxing
+        int p1 = i2.intValue();
+        
+        // AutoUnBoxing
+        int p2 = i2;
+        
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        
+        
+        System.out.println(i2.toString());
+        
+        System.out.println(i1.equals(i2));
+        
+        System.out.println(Integer.parseInt("123"));
+        
+        int result = i1.compareTo(i2);
+        
+        System.out.println(result);// i1 = -1 < 0 < 1
+        
+        Double d1 = Double.NaN;
+        
+        System.out.println(Double.isNaN(d1));
+        
+        
     }
 }
