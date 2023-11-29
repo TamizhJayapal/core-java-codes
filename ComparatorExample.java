822ffff9af72b7cd084c472a1c7709f2ee996e5e
Tamizhazhagan Jayapal <40054684+TamizhJayapal@users.noreply.github.com>
2023-11-29 20:29:49 +0530

    Update ComparatorExample.java

--- ComparatorExample.java
+++ ComparatorExample.java
@@ -1,61 +1,87 @@
-package com.object.sorting;
+package com.example;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
-public class ComparatorExample {
-
-public static class Student {
-        private int id;
-        private String name;
-
-        public Student(int id, String name) {
-            this.id = id;
-            this.name = name;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        @Override
-        public String toString() {
-            return "MyClass{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    '}';
-        }
-    }
-
-    public static class AgeComparator implements Comparator<Student> {
-        @Override
-        public int compare(Student obj1, Student obj2) {
-            return obj1.getId() - obj2.getId();
-        }
-    }
-
-public Main() {}
-
-public static void main(String[] args) {
-// Create a list of MyClass objects
-        List<Student> myObjects = new ArrayList<>();
-        myObjects.add(new Student(2, "B"));
-        myObjects.add(new Student(1, "A"));
-        myObjects.add(new Student(3, "C"));
-
-        // Sort the list using the custom comparator
-        Collections.sort(myObjects, new AgeComparator());
-
-        // Print the sorted list
-        for (Student obj : myObjects) {
-            System.out.println(obj);
-        }
+class Student {
+	private int id;
+	
+	private String name;
+	
+	private int age;
+	
+	public Student(int id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
 }
 
+class AgeComparator implements Comparator<Student> {
+	@Override
+	public int compare(Student o1, Student o2) {
+		return o1.getAge() - o2.getAge();
+	}
+}
+
+class NameComparator implements Comparator<Student> {
+
+	@Override
+	public int compare(Student o1, Student o2) {
+		return o1.getName().compareTo(o2.getName());
+	}
+	
+}
+
+
+
+public class ComparatorExample {
+	//using method reference
+	//Comparator<Student> byName = Comparator.comparing(Student::getName);
+
+	public static void main(String[] args) {
+		List<Student> myObjects = new ArrayList<>();
+		myObjects.add(new Student(2, "ram",35));
+		myObjects.add(new Student(1, "tamizh",25));
+		myObjects.add(new Student(3, "ashok",33));
+		myObjects.add(new Student(4, "ashok",22));
+		
+		Comparator<Student> nameComparator = new NameComparator();
+		Comparator<Student> ageComparator = new AgeComparator();
+		Comparator<Student> finalComparator = nameComparator.thenComparing(ageComparator);
+		
+		Collections.sort(myObjects, finalComparator);
+		
+		for (Student obj : myObjects) {
+			System.out.println(obj);
+		}
+	}
 }
